68a0b425a40a7329d575fe1711f1d69a974678e0
viclang <my.turn@aleeas.com>
2022-11-13 15:46:55 +0100

    Match

---
+++ src/InfinityComparable/Match.cs
@@ -0,0 +1,37 @@
+﻿namespace InfinityComparable
+{
+    public static partial class Infinity
+    {
+        public static void Match<T, TResult>(this Infinity<T> value, Action<T> finite, Action<bool> infinite)
+            where T : struct, IEquatable<T>, IComparable<T>, IComparable
+        {
+            if (value.IsInfinite)
+            {
+                infinite(value.positive);
+            }
+            else
+            {
+                finite(value.value);
+            }
+        }
+
+        public static TResult Match<T, TResult>(this Infinity<T> value, Func<T, TResult> finite, Func<bool, TResult> infinite)
+            where T : struct, IEquatable<T>, IComparable<T>, IComparable
+            => value.IsInfinite
+                ? infinite(value.positive)
+                : finite(value.value);
+
+
+        public static Task MatchAsync<T, TResult>(this Infinity<T> value, Func<T, Task> finite, Func<bool, Task> infinite)
+            where T : struct, IEquatable<T>, IComparable<T>, IComparable
+            => value.IsInfinite
+                ? infinite(value.positive)
+                : finite(value.value);
+
+        public static Task<TResult> MatchAsync<T, TResult>(this Infinity<T> value, Func<T, Task<TResult>> finite, Func<bool, Task<TResult>> infinite)
+            where T : struct, IEquatable<T>, IComparable<T>, IComparable
+            => value.IsInfinite
+                ? infinite(value.positive)
+                : finite(value.value);
+    }
+}
